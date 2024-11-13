a236ced7443f53a664d8942cbe0aa70b7bdd944f
Wiliam Bispo <wiliam_bis14@hotmail.com>
2024-11-12 23:25:46 -0300

    inserção do btnBackToTop

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+    const button = document.getElementById("backToTopBtn");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
